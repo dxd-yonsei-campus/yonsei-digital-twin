c3efd095306608306a799c109ae5d771d7bf2084
Lester Ong <lesterong776@gmail.com>
2025-05-19 18:20:38 +0800

    Add Avison Biomedical Research Center

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,16 +4,18 @@ import { glob } from "astro/loaders";
 const positionSchema = z.tuple([z.number(), z.number()]);
 
 const buildingsSchema = z.object({
-  id: z.number(),
+  id: z.number().or(z.string()),
   name: z.string(),
   name_en: z.string(),
   address: z.string().optional(),
   latitude: z.number(),
   longitude: z.number(),
-  geometry: z.object({
-    type: z.string(),
-    coordinates: z.array(positionSchema),
-  }),
+  geometry: z
+    .object({
+      type: z.string(),
+      coordinates: z.array(positionSchema),
+    })
+    .optional(),
 });
 
 const buildings = defineCollection({
