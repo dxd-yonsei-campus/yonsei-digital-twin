a8bea3d6b8bd8ddcce0b3e11f700bc9da08345fe
Lester Ong <lesterong776@gmail.com>
2025-05-19 17:33:33 +0800

    Update schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,18 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
+const positionSchema = z.tuple([z.number(), z.number()]);
+
 const buildingsSchema = z.object({
   id: z.number(),
   name: z.string(),
   name_en: z.string(),
   latitude: z.number(),
   longitude: z.number(),
+  geometry: z.object({
+    type: z.string(),
+    coordinates: z.array(positionSchema),
+  }),
 });
 
 const buildings = defineCollection({
