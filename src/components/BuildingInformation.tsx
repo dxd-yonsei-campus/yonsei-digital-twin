c3e1befdea33709e02b7078af928ab772eb5ef4c
Lester Ong <lesterong776@gmail.com>
2025-05-20 20:59:38 +0800

    Add building information panel

---
+++ src/components/BuildingInformation.tsx
@@ -0,0 +1,22 @@
+import { selectedId } from "@/store";
+import { useStore } from "@nanostores/react";
+import sinchonBuildings from "src/data/buildings/sinchon.json";
+
+const BuildingInformation = () => {
+  const $selectedId = useStore(selectedId);
+  const buildingData = sinchonBuildings.filter(
+    (building) => building.id === $selectedId,
+  );
+  const selectedBuilding =
+    buildingData.length >= 1 ? buildingData[0] : undefined;
+  return (
+    <div className="bg-background rounded z-10 absolute top-12 left-8 border p-3 min-w-84 shadow-xs">
+      <h2 className="font-bold text-lg">{selectedBuilding?.name_en}</h2>
+      <div className="text-sm text-muted-foreground">
+        {selectedBuilding?.name}
+      </div>
+    </div>
+  );
+};
+
+export default BuildingInformation;
--- src/store.ts
+++ src/store.ts
@@ -3,3 +3,4 @@ import { atom } from "nanostores";
 export type BuildingLayerType = "" | "osm";
 
 export const buildingLayer = atom<BuildingLayerType>("osm");
+export const selectedId = atom<string | number>("");
