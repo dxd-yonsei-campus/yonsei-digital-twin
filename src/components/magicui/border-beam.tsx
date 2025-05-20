afb20f9350a125c63adfd8b0db2e85c754dda3c3
Lester Ong <lesterong776@gmail.com>
2025-05-20 23:51:46 +0800

    Update design

--- src/components/magicui/border-beam.tsx
+++ src/components/magicui/border-beam.tsx
@@ -57,7 +57,7 @@ export const BorderBeam = ({
   initialOffset = 0,
 }: BorderBeamProps) => {
   return (
-    <div className="pointer-events-none absolute inset-0 rounded-md border border-transparent [mask-clip:padding-box,border-box] [mask-composite:intersect] [mask-image:linear-gradient(transparent,transparent),linear-gradient(#000,#000)]">
+    <div className="pointer-events-none absolute inset-0 rounded-sm border border-transparent [mask-clip:padding-box,border-box] [mask-composite:intersect] [mask-image:linear-gradient(transparent,transparent),linear-gradient(#000,#000)]">
       <motion.div
         className={cn(
           "absolute aspect-square",
