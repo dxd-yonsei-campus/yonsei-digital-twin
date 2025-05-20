3a4af9632f49cf3dd7f08709b66bcbc63e5bc045
lesterong <84223259+lesterong@users.noreply.github.com>
2025-05-20 23:56:05 +0800

    Add building info panel (#6)

---
+++ src/components/BuildingInformation.tsx
@@ -0,0 +1,42 @@
+import { selectedId } from "@/store";
+import { useStore } from "@nanostores/react";
+import sinchonBuildings from "@/data/buildings/sinchon.json";
+import { Button } from "@/components/ui/button";
+import { XIcon } from "lucide-react";
+
+const BuildingInformation = () => {
+  const $selectedId = useStore(selectedId);
+  const buildingData = sinchonBuildings.filter(
+    (building) => building.id === $selectedId,
+  );
+  const selectedBuilding = buildingData.length >= 1 ? buildingData[0] : null;
+
+  if (!selectedBuilding) {
+    return null;
+  }
+
+  return (
+    <div className="rounded z-10 absolute top-12 left-0 px-4 w-108 max-w-full md:px-8">
+      <div className="bg-background border p-3 shadow-xs">
+        <div className="flex justify-between gap-1">
+          <div>
+            <h2 className="font-bold text-lg/6">{selectedBuilding.name_en}</h2>
+            <div className="text-sm text-muted-foreground">
+              {selectedBuilding.name}
+            </div>
+          </div>
+          <Button
+            onClick={() => selectedId.set("")}
+            className="relative left-1.5 bottom-1.5 text-muted-foreground"
+            variant="ghost"
+            size="icon"
+          >
+            <XIcon />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BuildingInformation;
