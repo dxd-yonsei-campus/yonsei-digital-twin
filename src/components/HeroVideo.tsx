8a780b83dad13c1bce611c6c2ccbf660740fcb5c
Lester Ong <lesterong776@gmail.com>
2025-05-21 00:33:36 +0800

    Fix styles

---
+++ src/components/HeroVideo.tsx
@@ -0,0 +1,25 @@
+import { BorderBeam } from "@/components/magicui/border-beam";
+import { Card, CardContent } from "@/components/ui/card";
+
+const HeroVideo = () => {
+  return (
+    <Card className="relative overflow-hidden max-w-3xl py-2">
+      <CardContent className="px-2">
+        <video
+          className="w-full aspect-video object-fill rounded-lg"
+          src="/hero-video.mp4"
+          autoPlay
+          muted
+          loop
+        ></video>
+      </CardContent>
+      <BorderBeam
+        duration={8}
+        size={400}
+        className="from-transparent via-primary to-transparent"
+      />
+    </Card>
+  );
+};
+
+export default HeroVideo;
