170823ac2507a4f9f88d60689c8e54d9ae8c072c
Lester Ong <lesterong776@gmail.com>
2025-05-25 00:11:22 +0800

    Update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,38 @@
+// @ts-check
+import eslint from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
 
-export default [
+export default tseslint.config(
+  {
+    ignores: [
+      "**/dist",
+      "**/node_modules",
+      "**/.astro",
+      "**/.github",
+      "**/.changeset",
+    ],
+  },
+
+  // JavaScript
+  eslint.configs.recommended,
+  // TypeScript
+  ...tseslint.configs.recommended,
+
+  // Allow triple-slash references in `*.d.ts` files.
+  {
+    files: ["**/*.d.ts"],
+    rules: {
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
+
+  // Astro
   ...eslintPluginAstro.configs.recommended,
   {
     rules: {
       // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+      "astro/no-set-html-directive": "error",
     },
   },
-];
+);
