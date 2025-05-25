23f6f7c3f97910cf62b70ec834cd02890d58e718
Lester Ong <lesterong776@gmail.com>
2025-05-25 18:38:26 +0800

    Use persistent atom

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,10 @@
 import { atom } from 'nanostores';
+import { persistentAtom } from '@nanostores/persistent';
 import type { CampusName, BuildingLayerType } from '@/types/map';
 
 export const buildingLayer = atom<BuildingLayerType>('osm');
 export const selectedId = atom<string | number>('');
-export const selectedCampus = atom<CampusName>('sinchon');
+export const selectedCampus = persistentAtom<CampusName>('campus', 'sinchon', {
+  encode: JSON.stringify,
+  decode: JSON.parse,
+});
