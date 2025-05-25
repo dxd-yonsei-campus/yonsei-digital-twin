e0b3b3f9f9682f469ad5c706f0e33a6be989a3bf
Lester Ong <lesterong776@gmail.com>
2025-05-26 00:55:37 +0800

    Set up translations structure

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+  en: 'English',
+  ko: 'Korean',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'home.title': 'Yonsei Digital Twin',
+  },
+  ko: {
+    'home.title': '연세 디지털 트윈',
+  },
+} as const;
