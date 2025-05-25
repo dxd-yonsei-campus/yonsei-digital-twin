c3d84abaaa6e92c14aa2ee3ed9ba2378ae0e8d2e
Lester Ong <lesterong776@gmail.com>
2025-05-26 02:27:03 +0800

    Add translation for map

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,6 +10,7 @@ export const ui = {
     'site.title': 'Yonsei Digital Twin',
     'site.description':
       "Discover Yonsei University's campuses and access detailed building information through the Yonsei Digital Twin.",
+    'site.map': 'Map',
     'home.description': 'A digital twin of the Yonsei University Campus.',
     'search.placeholder': 'Search for buildings...',
     sinchon: 'Sinchon',
@@ -21,6 +22,7 @@ export const ui = {
     'site.title': '연세 디지털 트윈',
     'site.description':
       '연세대학교 캠퍼스를 둘러보고 연세 디지털 트윈을 통해 자세한 건물 정보를 확인해 보세요',
+    'site.map': '지도',
     'home.description': '연세대학교 캠퍼스의 디지털 트윈',
     'search.placeholder': '건물 검색',
     sinchon: '신촌',
