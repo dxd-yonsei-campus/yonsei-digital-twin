1677569ecd848f2e4ef0a37f1dccfc1c5232a876
Lester Ong <lesterong776@gmail.com>
2025-05-26 01:11:30 +0800

    Add translations for home page

--- src/components/landing/MapLink.tsx
+++ src/components/landing/MapLink.tsx
@@ -1,21 +1,25 @@
 import { Button } from '@/components/ui/button';
-import { campusNameToDisplayableName } from '@/lib/mapUtils';
 import { selectedCampus } from '@/store';
 import type { CampusName } from '@/types/map';
+import { useTranslations } from '@/i18n/utils';
+import type { ui } from '@/i18n/ui';
 
 type MapLinkProps = {
   campus: CampusName;
+  lang: keyof typeof ui;
 };
 
-const MapLink = ({ campus }: MapLinkProps) => {
+const MapLink = ({ campus, lang }: MapLinkProps) => {
+  const t = useTranslations(lang);
+
   return (
     <Button
       variant="default"
       className="w-full cursor-pointer sm:w-fit"
       asChild
     >
-      <a onClick={() => selectedCampus.set(campus)} href="/map">
-        {campusNameToDisplayableName[campus]} Campus
+      <a onClick={() => selectedCampus.set(campus)} href={`/${lang}/map`}>
+        {t(`${campus}_long`)}
       </a>
     </Button>
   );
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,8 +8,16 @@ export const defaultLang = 'en';
 export const ui = {
   en: {
     'home.title': 'Yonsei Digital Twin',
+    sinchon: 'Sinchon',
+    sinchon_long: 'Sinchon Campus',
+    songdo: 'Songdo',
+    songdo_long: 'Songdo Campus',
   },
   ko: {
     'home.title': '연세 디지털 트윈',
+    sinchon: '신촌',
+    sinchon_long: '신촌캠퍼스',
+    songdo: '송도',
+    songdo_long: '송도캠퍼스',
   },
 } as const;
