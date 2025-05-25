d835e2091b49cf0b705971458d5110186ed2e6ee
Lester Ong <lesterong776@gmail.com>
2025-05-25 17:59:58 +0800

    Add dropdown

--- src/components/ResetButtonGroup.tsx
+++ src/components/ResetButtonGroup.tsx
@@ -1,17 +1,48 @@
 import { ButtonGroup } from '@/components/ui/button-group';
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { ChevronUp, HomeIcon } from 'lucide-react';
+import { useStore } from '@nanostores/react';
+import { selectedCampus } from '@/store';
+import type { CampusName } from '@/types/map';
+import { campusNameToDisplayableName } from '@/lib/mapUtils';
 
 const ResetButtonGroup = () => {
+  const $selectedCampus = useStore(selectedCampus);
   return (
     <ButtonGroup>
       <Button variant="outline" id="reset-view">
         <HomeIcon />
-        Sinchon
+        {campusNameToDisplayableName[$selectedCampus]}
       </Button>
-      <Button variant="outline" size="icon">
-        <ChevronUp />
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <ChevronUp />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuRadioGroup
+            value={$selectedCampus}
+            onValueChange={(campus) => selectedCampus.set(campus as CampusName)}
+          >
+            {['sinchon', 'songdo'].map((name) => {
+              const campus = name as CampusName;
+              return (
+                <DropdownMenuRadioItem key={name} value={campus}>
+                  {campusNameToDisplayableName[campus]}
+                </DropdownMenuRadioItem>
+              );
+            })}
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </ButtonGroup>
   );
 };
--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
 import { atom } from 'nanostores';
-import type { BuildingLayerType } from '@/types/map';
+import type { CampusName, BuildingLayerType } from '@/types/map';
 
 export const buildingLayer = atom<BuildingLayerType>('osm');
 export const selectedId = atom<string | number>('');
+export const selectedCampus = atom<CampusName>('sinchon');
