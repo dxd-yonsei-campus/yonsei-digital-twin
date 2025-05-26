ecd7f4d33c5d8795ed88a7026ad4c1154d824ed6
Lester Ong <lesterong776@gmail.com>
2025-05-26 11:10:50 +0800

    Remove unused configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,11 +11,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://yonsei-campus.com',
 
-  i18n: {
-    locales: ['en', 'ko'],
-    defaultLocale: 'en',
-  },
-
   vite: {
     plugins: [tailwindcss()],
   },
