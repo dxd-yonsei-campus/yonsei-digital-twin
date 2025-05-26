805b2e643bb6a86e95ae2e2cec47e6ce4b353a11
lesterong <84223259+lesterong@users.noreply.github.com>
2025-05-27 02:40:14 +0800

    Refactor language picker (#19)

---
+++ src/components/landing/LanguagePicker.tsx
@@ -0,0 +1,57 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { languages, ui } from '@/i18n/ui';
+import { Button } from '../ui/button';
+import { cn } from '@/lib/utils';
+
+type LanguagePickerProps = {
+  className?: string;
+  redirectPath?: string;
+  children?: React.ReactElement;
+  align?: 'center' | 'start' | 'end' | undefined;
+  lang: keyof typeof ui;
+};
+
+const LanguagePicker = ({
+  className,
+  children,
+  redirectPath,
+  align = 'start',
+  lang: currentLang,
+}: LanguagePickerProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className={className} asChild>
+        <Button variant="ghost">{children ? children : `Language`}</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align={align}
+        className={cn(
+          'w-34',
+          { 'mr-4': align === 'end' },
+          { 'ml-4': align === 'start' },
+        )}
+      >
+        <DropdownMenuRadioGroup
+          value={currentLang}
+          onValueChange={(lang) => {
+            window.location.href = `/${lang}/${redirectPath || ''}`;
+          }}
+        >
+          {Object.entries(languages).map(([lang, label]) => (
+            <DropdownMenuRadioItem key={lang} value={lang}>
+              {label}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default LanguagePicker;
