1075f36aed780ac3a555c5fd405422d9e1337345
Lester Ong <lesterong776@gmail.com>
2025-05-27 03:28:42 +0800

    Add theme toggle

---
+++ src/components/ThemeIcon.tsx
@@ -0,0 +1,46 @@
+import { Moon, Sun } from 'lucide-react';
+import { Button } from './ui/button';
+import { theme } from '@/store';
+import { useStore } from '@nanostores/react';
+import React, { useEffect } from 'react';
+import { cn } from '@/lib/utils';
+
+const ThemeIcon = () => {
+  const currentTheme = useStore(theme);
+  const [isRendered, setIsRendered] = React.useState(false);
+
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
+
+  if (!isRendered) {
+    return <Button variant="ghost" size="icon"></Button>;
+  }
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => theme.set(currentTheme === 'light' ? 'dark' : 'light')}
+    >
+      <svg
+        className={cn('size-5.5')}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <Sun
+          className={cn(
+            'rotate-0 opacity-100 transition-all duration-300 dark:rotate-90 dark:opacity-0',
+          )}
+        />
+        <Moon
+          className={cn(
+            'rotate-90 opacity-0 transition-all duration-300 dark:rotate-0 dark:opacity-100',
+          )}
+        />
+      </svg>
+    </Button>
+  );
+};
+
+export default ThemeIcon;
--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,6 @@ import type { CampusName, BuildingLayerType } from '@/types/map';
 
 export const buildingLayer = atom<BuildingLayerType>('osm');
 export const selectedId = atom<string | number>('');
-export const selectedCampus = persistentAtom<CampusName>('campus', 'sinchon', {
-  encode: JSON.stringify,
-  decode: JSON.parse,
-});
+export const selectedCampus = persistentAtom<CampusName>('campus', 'sinchon');
+
+export const theme = persistentAtom<'light' | 'dark'>('theme', 'light');
