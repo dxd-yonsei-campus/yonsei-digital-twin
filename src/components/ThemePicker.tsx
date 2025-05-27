33022ff6e0e7cd7b4655eebde1e27064c0f578d0
Lester Ong <lesterong776@gmail.com>
2025-05-27 12:27:01 +0800

    Fix rotation issue

--- src/components/ThemePicker.tsx
+++ src/components/ThemePicker.tsx
@@ -3,7 +3,6 @@ import { Button } from './ui/button';
 import { theme } from '@/store';
 import { useStore } from '@nanostores/react';
 import React, { useEffect } from 'react';
-import { cn } from '@/lib/utils';
 
 type ThemePickerProps = {
   className?: string;
@@ -28,22 +27,8 @@ const ThemePicker = ({ className }: ThemePickerProps) => {
       onClick={() => theme.set(currentTheme === 'light' ? 'dark' : 'light')}
       className={className}
     >
-      <svg
-        className={cn('size-5.5')}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-      >
-        <Sun
-          className={cn(
-            'rotate-0 opacity-100 transition-all duration-300 dark:rotate-90 dark:opacity-0',
-          )}
-        />
-        <Moon
-          className={cn(
-            'rotate-90 opacity-0 transition-all duration-300 dark:rotate-0 dark:opacity-100',
-          )}
-        />
-      </svg>
+      <Sun className="absolute size-5.5 rotate-0 opacity-100 transition-all duration-300 dark:rotate-45 dark:opacity-0" />
+      <Moon className="absolute size-5.5 rotate-45 opacity-0 transition-all duration-300 dark:rotate-0 dark:opacity-100" />
     </Button>
   );
 };
