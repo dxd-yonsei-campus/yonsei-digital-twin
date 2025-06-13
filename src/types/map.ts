36d5b3cb4f81679dc6e3d13419f42f7a9c49cfd1
Lester Ong <lesterong776@gmail.com>
2025-06-13 09:39:22 +0800

    Hide MIRAE

--- src/types/map.ts
+++ src/types/map.ts
@@ -1,4 +1,8 @@
 export type BuildingLayerType = '' | 'osm';
 
-export const campuses = <const>['sinchon', 'songdo', 'mirae'];
+export const campuses = <const>[
+  'sinchon',
+  'songdo',
+  // 'mirae', // Enable this when Mirae campus is ready
+];
 export type CampusName = (typeof campuses)[number];
