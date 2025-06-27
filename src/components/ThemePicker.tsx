49ca2b64867e0f5c5ac09d4cbaf65d4225c41b80
lesterong <84223259+lesterong@users.noreply.github.com>
2025-06-27 19:28:40 +0800

    Improve accessibility (#33)

--- src/components/ThemePicker.tsx
+++ src/components/ThemePicker.tsx
@@ -16,6 +16,7 @@ const ThemePicker = ({ className }: ThemePickerProps) => {
       size="icon"
       onClick={() => theme.set(currentTheme === 'light' ? 'dark' : 'light')}
       className={className}
+      aria-label="theme"
     >
       <Sun className="absolute size-5.5 rotate-0 opacity-100 transition-all duration-300 dark:rotate-45 dark:opacity-0" />
       <Moon className="absolute size-5.5 rotate-45 opacity-0 transition-all duration-300 dark:rotate-0 dark:opacity-100" />
