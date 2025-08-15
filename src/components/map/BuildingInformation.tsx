e62c1cc45ec863d129ffc2b6e87a3d31c973c683
rubberquackingduck <222044594+rubberquackingduck@users.noreply.github.com>
2025-08-15 21:38:58 +0800

    Use building layers to render

--- src/components/map/BuildingInformation.tsx
+++ src/components/map/BuildingInformation.tsx
@@ -1,4 +1,4 @@
-import { selectedId } from '@/store';
+import { buildingLayer, selectedId } from '@/store';
 import { useStore } from '@nanostores/react';
 import {
   Dialog,
@@ -39,6 +39,7 @@ type BuildingInformationProps = {
 
 const BuildingInformation = ({ lang }: BuildingInformationProps) => {
   const $selectedId = useStore(selectedId);
+  const $buildingLayer = useStore(buildingLayer);
   const [displayBuilding, setDisplayBuilding] = useState<BuildingProps | null>(
     null,
   );
@@ -119,11 +120,15 @@ const BuildingInformation = ({ lang }: BuildingInformationProps) => {
             </DialogDescription>
           </DialogHeader>
           <CollapsibleContent className="max-h-[52vh] space-y-4 overflow-scroll [&>:first-child]:pt-5">
-            <ConstructionInformation
-              lang={lang}
-              building={displayBuilding}
-              images={resolvedImages}
-            />
+            {$buildingLayer !== 'rhino-simple' ? (
+              <ConstructionInformation
+                lang={lang}
+                building={displayBuilding}
+                images={resolvedImages}
+              />
+            ) : (
+              <div>Energy Use</div>
+            )}
           </CollapsibleContent>
           <CollapsibleTrigger asChild>
             <button className="absolute top-4 right-11 rounded-xs opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:ring-2 focus:ring-ring focus:ring-offset-2 focus:outline-hidden [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4.5">
