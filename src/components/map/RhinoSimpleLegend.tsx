3a29199ac16ec4baf92abd90c6375234a79fb680
rubberquackingduck <222044594+rubberquackingduck@users.noreply.github.com>
2025-09-03 23:08:48 +0800

    Support lang

--- src/components/map/RhinoSimpleLegend.tsx
+++ src/components/map/RhinoSimpleLegend.tsx
@@ -1,21 +1,30 @@
+import type { ui } from '@/i18n/ui';
+import { useTranslations } from '@/i18n/utils';
+import { cn } from '@/lib/utils';
 import { buildingLayer } from '@/store';
 import { useStore } from '@nanostores/react';
 
-const RhinoSimpleLegend = () => {
+type RhinoSimpleLegendProps = {
+  lang: keyof typeof ui;
+};
+
+const RhinoSimpleLegend = ({ lang }: RhinoSimpleLegendProps) => {
   const $buildingLayer = useStore(buildingLayer);
   const isRhinoSimple = $buildingLayer === 'rhino-simple';
+  const t = useTranslations(lang);
 
   return (
     <div
-      className={`fixed right-0 bottom-21 z-20 w-full transform transition-all duration-150 ease-out xs:w-80 ${
+      className={cn(
+        'fixed right-0 bottom-21 z-20 w-full transform transition-all duration-150 ease-out xs:w-80',
         isRhinoSimple
           ? 'translate-y-0 scale-100 opacity-100'
-          : 'pointer-events-none translate-y-0.5 scale-95 opacity-0'
-      }`}
+          : 'pointer-events-none translate-y-0.5 scale-95 opacity-0',
+      )}
     >
       <div className="mx-4 rounded-md main-bg px-3.5 py-2.5 shadow-sm">
         <div className="mb-1 text-xs text-muted-foreground">
-          Energy Use (kWh/m<sup>2</sup>)
+          {t('energy_use_intensity_long')} (kWh/m<sup>2</sup>)
         </div>
         <div
           className="h-4 w-full rounded bg-white"
