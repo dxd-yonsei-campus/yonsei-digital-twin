4b3d545bbf6b42b6fdf74d8b3ce8ef3338360cb8
rubberquackingduck <222044594+rubberquackingduck@users.noreply.github.com>
2025-09-27 00:44:34 +0800

    Refactor

--- src/components/map/EnergyUseInformation.tsx
+++ src/components/map/EnergyUseInformation.tsx
@@ -10,18 +10,12 @@ import { useStore } from '@nanostores/react';
 import { cn } from '@/lib/utils';
 import type { ui } from '@/i18n/ui';
 import type { CollectionEntry } from 'astro:content';
-import EnergyChart from './building-info/EnergyChart';
-import { getAllBuildings, getBuildingWithId } from '@/lib/mapApi';
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from '@/components/ui/collapsible';
-import { ChevronRight, XIcon } from 'lucide-react';
+import { getAllBuildings } from '@/lib/mapApi';
 import { useState } from 'react';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { useTranslations } from '@/i18n/utils';
 import YearlyEnergyChart from './building-info/YearlyEnergyChart';
+import MonthlyEnergyCharts from './building-info/MonthlyEnergyCharts';
 
 type MonthlyEnergyUseEntry = CollectionEntry<'monthlyEnergyUse'>;
 
@@ -146,7 +140,7 @@ const EnergyUseInformation = ({
               <div className="mt-2">
                 <div className="space-y-4">
                   {$selectedIdsForEnergyUse.map((id) => (
-                    <MonthlyEnergyUseInformation
+                    <MonthlyEnergyCharts
                       key={id}
                       id={id}
                       lang={lang}
@@ -168,68 +162,3 @@ const EnergyUseInformation = ({
 };
 
 export default EnergyUseInformation;
-
-type MonthlyEnergyUseInformationProps = {
-  lang: keyof typeof ui;
-  id: string | number;
-  energyUseType: 'eu' | 'eui';
-  monthlyEnergyUseCollection: MonthlyEnergyUseEntry[];
-};
-
-const MonthlyEnergyUseInformation = ({
-  lang,
-  id,
-  monthlyEnergyUseCollection,
-  energyUseType,
-}: MonthlyEnergyUseInformationProps) => {
-  const $selectedIdsForEnergyUse = useStore(selectedIdsForEnergyUse);
-  const buildingData = getBuildingWithId(id);
-  const totalFloorArea = buildingData?.total_floor_area;
-  const monthlyEnergyUseId = String(buildingData?.monthly_energy_use);
-  const monthlyEnergyUse = monthlyEnergyUseCollection.find(
-    (data) => String(data.id) === String(monthlyEnergyUseId),
-  )?.data;
-  const nameToDisplay =
-    lang === 'ko' ? buildingData?.name : buildingData?.name_en;
-  const handleRemoveId = () =>
-    selectedIdsForEnergyUse.set(
-      $selectedIdsForEnergyUse.filter((selectedId) => selectedId !== id),
-    );
-  const hasErrorMessage = energyUseType === 'eui' && !totalFloorArea;
-
-  return (
-    <Collapsible
-      key={id}
-      className={cn('group', hasErrorMessage && 'eui-error')}
-    >
-      <div className="mb-2.5 flex w-full items-center justify-between">
-        <CollapsibleTrigger asChild>
-          <button className="w-full text-sm font-medium text-foreground/85 hover:text-foreground">
-            <div className="flex items-center gap-1.5">
-              <ChevronRight className="size-3.5 transition-transform duration-200 group-data-[state=open]:rotate-90" />
-              <div className="text-left">
-                {nameToDisplay}
-                {hasErrorMessage && '*'}
-              </div>
-            </div>
-          </button>
-        </CollapsibleTrigger>
-        <button
-          className="text-muted-foreground hover:text-foreground"
-          onClick={handleRemoveId}
-        >
-          <XIcon className="size-4" />
-        </button>
-      </div>
-      <CollapsibleContent>
-        <EnergyChart
-          lang={lang}
-          chartData={monthlyEnergyUse}
-          totalFloorArea={totalFloorArea}
-          energyUseType={energyUseType}
-          className="h-[200px]"
-        />
-      </CollapsibleContent>
-    </Collapsible>
-  );
-};
