074cd5d2f108a90abceb9f55271c4b0177f189d3
rubberquackingduck <222044594+rubberquackingduck@users.noreply.github.com>
2025-09-27 20:07:40 +0800

    Remove unused comments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,7 +11,6 @@ export function getLongestLineLengthForMaxLines(
 ) {
   const words = phrase.split(' ');
 
-  // Helper: Try to fit words into `maxLines` lines with lineLength limit
   function canFit(lineLength: number) {
     let linesUsed = 1;
     let currentLineLength = 0;
@@ -24,7 +23,7 @@ export function getLongestLineLengthForMaxLines(
       if (currentLineLength === 0) {
         currentLineLength = word.length;
       } else if (currentLineLength + 1 + word.length <= lineLength) {
-        currentLineLength += 1 + word.length; // +1 for space
+        currentLineLength += 1 + word.length;
       } else {
         linesUsed++;
         currentLineLength = word.length;
