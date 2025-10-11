7f8c2bc98cf75586f49f3cacb551d5bcc3246cf8
rubberquackingduck <222044594+rubberquackingduck@users.noreply.github.com>
2025-10-11 22:12:45 +0800

    Combine college of music

--- src/lib/mapUtils.ts
+++ src/lib/mapUtils.ts
@@ -2,8 +2,13 @@ import type { BuildingIdType } from '@/types/map';
 
 const COLLEGE_OF_MEDICINE_IDS: Array<BuildingIdType> = [1178739301, 1178739302];
 const LIBERTAS_HALL_A_IDS: Array<BuildingIdType> = [1096854782, 478878023];
+const COLLEGE_OF_MUSIC_IDS: Array<BuildingIdType> = [429077789, 429077790];
 
-const BUILDING_GROUPS = [COLLEGE_OF_MEDICINE_IDS, LIBERTAS_HALL_A_IDS];
+const BUILDING_GROUPS = [
+  COLLEGE_OF_MEDICINE_IDS,
+  LIBERTAS_HALL_A_IDS,
+  COLLEGE_OF_MUSIC_IDS,
+];
 
 const BUILDING_LOOKUP = new Map<BuildingIdType, Array<BuildingIdType>>();
 
